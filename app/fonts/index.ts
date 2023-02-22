f923a3b2492f44dd816c58a42a67a74bf1bd3a42
Nemanja Vucic <nemanja.vucic@htecgroup.com>
2023-02-22 12:47:04 +0100

    fix: global styles with variables

--- app/fonts/index.ts
+++ app/fonts/index.ts
@@ -1,4 +1,5 @@
 import {Inter,Roboto_Mono,Fraunces,Cormorant} from '@next/font/google';
+import localFont from '@next/font/local';
 
 export const inter=Inter({subsets:['latin'],display:'swap'});
 
@@ -7,4 +8,7 @@ export const roboto_mono=Roboto_Mono({subsets:['latin'],display:'swap'});
 // fonts with variables
 export const fraunces=Fraunces({subsets:['latin'],display:'swap', variable:'--font-fraunces'});
 export const cormorant=Cormorant({subsets:['latin'],display:'swap', variable:'--font-cormorant'});
+
+// local fonts
+export const signika=localFont({src:'./local-fonts/signika.ttf',style:'italic'})
 
--- app/layout.tsx
+++ app/layout.tsx
@@ -8,7 +8,7 @@ export default function RootLayout({
 }) {
   return (
     // set fonts globally
-    <html lang="en" className={`${inter.className} ${fraunces.className} ${cormorant.className}`}>
+    <html lang="en" className={`${inter.className} ${fraunces.variable} ${cormorant.variable}`}>
       {/*
         <head /> will contain the components returned by the nearest parent
         head.tsx. Find out more at https://beta.nextjs.org/docs/api-reference/file-conventions/head
