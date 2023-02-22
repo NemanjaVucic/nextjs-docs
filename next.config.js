7bee97ea6f659538cc50fdc335db074d7cfeddf7
Nemanja Vucic <nemanja.vucic@htecgroup.com>
2023-02-22 13:48:04 +0100

    feat: nextjs config

--- next.config.js
+++ next.config.js
@@ -1,8 +1,10 @@
 const path=require('path');
+const withMDX = require('@next/mdx');
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   // sassOptions: {includePaths: [path.join(__dirname,'styles')]},
+  pageExtensions:['tsx','ts','jsx','js','mdx'],
   images: {
     remotePatterns: [
       // setup your remote image domains here
@@ -16,15 +18,18 @@ const nextConfig = {
   },
   experimental: {
     appDir: true,
-    // typedRoutes:true,
+    mdxRs:true,
+    typedRoutes:true,
+    serverComponentsExternalPackages:['@acme/ui'],
     // If both are configured, the subset in the function call is used & this one will be ignored.
     fontLoaders:[{
       loader:'@next/font/google',
       options:{
         subsets:['latin']
       }
-    }]
+    }],
   },
+  transpilePackages:['@acme/ui','loadash-es'],
 }
 
-module.exports = nextConfig
+module.exports = withMDX(nextConfig)
